a785f3ff95f7dbfe5e56a47afe4f864d9ad3e288
Isaac Fierro <zifg_36@live.com.mx>
2022-07-09 09:00:28 -0700

    Credenciales de la base de datos añadidas

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,7 +6,11 @@ ALLOWED_HOSTS = ['proyectomoises.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8a6i3oarqp0ki',
+        'USER': 'zmtrjsodzyeijv',
+        'PASSWORD': '2b998cd5b434683e9d981d43087c18eb80ca197adfc120deb1732d07d59b5088',
+        'HOST': 'ec2-34-233-115-14.compute-1.amazonaws.com',
+        'PORT': 5432
     }
 }
