69ccc4c316041a9feb4f83cf1afb774c60a7b679
Isaac Fierro <zifg_36@live.com.mx>
2022-08-06 18:29:02 -0700

    dsfdsffsd

--- apps/modulo/urls.py
+++ apps/modulo/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import listar_modulos, crear_modulo, eliminar_modulo, ModuloListCreateAPIView
+from .views import crear_modulo, eliminar_modulo, ModuloListCreateAPIView
 
 
 urlpatterns = [
-    path('', listar_modulos, name='listado'),
     path('crear/', crear_modulo, name='crear'),
     path('eliminar/<id>/', eliminar_modulo, name='eliminar'),
     path('api/', ModuloListCreateAPIView.as_view(), name='listar-crear')
